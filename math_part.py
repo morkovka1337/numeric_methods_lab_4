1d1fb25c0acb075a4382f5e5796f27db3f30f77d
Vladimir Loginov <morkovka1337@users.noreply.github.com>
2018-12-09 21:42:08 +0300

    fixed a_func, d_func, phi, Thomas algorithm

--- math_part.py
+++ math_part.py
@@ -9,25 +9,12 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import QtWidgets, QtGui, QtCore
 import matplotlib.pyplot as plt
 
-def TDMASolve(a, c, b, d):
-    alpha = [0]
-    beta = [0]
-    x = np.zeros(len(d))
-    x[0] = d[0]
-    alpha[0] = 0
-    beta[0] = d[0]
-    for i in range(1, len(d)):
-        alpha.append(b[i-1]/(c[i-1]-alpha[i-1]*a[i-1]))
-        beta.append((d[i] + beta[i-1]*a[i-1])/c[i-1]-alpha[i-1]*a[i-1])
-    x[len(d)-1] = d[len(d)-1]
-    x[len(d)-2] = b[len(d)-2] + alpha[len(d)-2]*d[len(d)-1]
-    for i in range (len(d)-2, -1, -1):
-        x[i] = alpha[i+1] * x[i+1] + beta[i+1]
-    return x
+
 
 
 class mathpart(Ui_MainWindow):
 
+ 
 
     def build_test_1(self, n):
 
@@ -89,27 +76,42 @@ class mathpart(Ui_MainWindow):
 
     def build_test_2(self, n):
         ksi = np.pi/4
-        # def k(x):
-        #     return 1 if x < ksi else 2
-        # def q(x):
-        #     return ksi + 1 if x < ksi else 2*np.pi**2
-        # def f(x):
-        #     return 1 if x < ksi else np.sqrt(2)/2
+        def TDMASolve(a, c, b, d):
+            nonlocal n
+            alpha = np.zeros(n)
+            beta = np.zeros(n)
+            x = np.zeros(n+1)
+            x[0] = d[0] #x[0] = kapa1*x[1] + mu1, mu1 = d[0]
+            alpha[0] = 0 # alpha[0] = kapa1, kapa1 в этой задаче всегда 0
+            beta[0] = d[0] # beta[0] = mu[1]
+            for i in range(0, n-1):
+                alpha[i+1] = b[i]/(c[i]-alpha[i]*a[i])
+                # phi[i] = d[i+1]
+                beta[i+1] = (d[i+1] * beta[i] * a[i])/(c[i] - alpha[i] * a[i])
+                # alpha.append(b[i-1]/(c[i-1]-alpha[i-1]*a[i-1]))
+                # beta.append((d[i] + beta[i-1]*a[i-1])/c[i-1]-alpha[i-1]*a[i-1])
+            
+            plt.subplot(211)
+            plt.plot(beta)
+            x[-1] = d[-1]
+            for i in range (n-1,  0, -1):
+                x[i] = alpha[i] * x[i+1] + beta[i]
+            return x
 
         def a_func(x):
             nonlocal h
-            if x < ksi:
+            if x <= ksi:
                 return 1
-            elif x-h > ksi:
+            elif x-h >= ksi:
                 return 2
-            elif x-h<ksi:
-                return (ksi-x+h) + (x-ksi)*2
+            elif x-h<ksi and x > ksi:
+                return h/((ksi-x)/2 + h)
         def d_func(x):
             nonlocal h
             if x+h/2 <= ksi:
                 return ksi + 1
             elif x-h/2 < ksi and x+h/2 > ksi:
-                return (ksi+1)*(ksi-x+h/2) + np.pi**2*(x+h/2-ksi)/2
+                return (ksi+1)*(ksi-x+h/2)/h + np.pi**2*(x+h/2-ksi)/(2*h)
             elif x-h/2 >= ksi:
                 return np.pi**2/2
             
@@ -118,7 +120,7 @@ class mathpart(Ui_MainWindow):
             if x+h/2 <= ksi:
                 return 1
             elif x-h/2 < ksi and x+h/2 > ksi:
-                return (ksi-x+h/2) + (np.sqrt(2)/2)*(x+h/2-ksi)
+                return (ksi-x+h/2)/h + np.sqrt(2)*(x+h/2-ksi)/(2*h)
             elif x-h/2 >= ksi:
                 return np.sqrt(2)/2
 
@@ -141,15 +143,15 @@ class mathpart(Ui_MainWindow):
 
         # syst = np.zeros((n+1, n+1))
         # syst[0][0] = 1
-        C = np.zeros(n)
-        A = np.zeros(n)
-        B = np.zeros(n)
-        for i in range(0, n):
-            xi = (i+1)*h
-            xi1 = (i+2)*h
-            A[i] = a_func(xi)/h**2
-            C[i] = -(a_func(xi)/(h**2)+d_func(xi)+a_func(xi1)/(h**2))
-            B[i] = a_func(xi1)/h**2
+        C = np.zeros(n-1)
+        A = np.zeros(n-1)
+        B = np.zeros(n-1)
+        for i in range(1, n): #последний индекс - n-1
+            xi = (i)*h
+            xi1 = (i+1)*h
+            A[i-1] = a_func(xi)/h**2
+            C[i-1] = -(a_func(xi)/(h**2)+d_func(xi)+a_func(xi1)/(h**2))
+            B[i-1] = a_func(xi1)/h**2
         # syst[n][n] = 0
 
         # v = np.linalg.solve(syst, d)
@@ -162,7 +164,8 @@ class mathpart(Ui_MainWindow):
         # c[n] = 1
 
         v = TDMASolve(A, C, B, d)
-        plt.subplot(111)
+        
+        plt.subplot(212)
         #plt.plot(u)
         plt.ylabel("Температура")
         plt.plot(v)
