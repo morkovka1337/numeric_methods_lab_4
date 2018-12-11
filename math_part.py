again fixed thomas algorithm, fixed calculation of the a_func

math_part.py
@@ -78,24 +78,22 @@ class mathpart(Ui_MainWindow):
         ksi = np.pi/4
         def TDMASolve(a, c, b, d):
             nonlocal n
-            alpha = np.zeros(n)
-            beta = np.zeros(n)
+            alpha = np.zeros(n-1)
+            beta = np.zeros(n-1)
             x = np.zeros(n+1)
-            x[0] = d[0] #x[0] = kapa1*x[1] + mu1, mu1 = d[0]
+            #x[0] = d[0] #x[0] = kapa1*x[1] + mu1, mu1 = d[0]
             alpha[0] = 0 # alpha[0] = kapa1, kapa1 в этой задаче всегда 0
             beta[0] = d[0] # beta[0] = mu[1]
-            for i in range(0, n-1):
+            for i in range(0, n-2):
                 alpha[i+1] = b[i]/(c[i]-alpha[i]*a[i])
                 # phi[i] = d[i+1]
-                beta[i+1] = (d[i+1] * beta[i] * a[i])/(c[i] - alpha[i] * a[i])
+                beta[i+1] = (d[i+1] + beta[i] * a[i])/(c[i] - alpha[i] * a[i])
                 # alpha.append(b[i-1]/(c[i-1]-alpha[i-1]*a[i-1]))
                 # beta.append((d[i] + beta[i-1]*a[i-1])/c[i-1]-alpha[i-1]*a[i-1])
-            
-            plt.subplot(211)
-            plt.plot(beta)
+
             x[-1] = d[-1]
-            for i in range (n-1,  0, -1):
-                x[i] = alpha[i] * x[i+1] + beta[i]
+            for i in range (n-1,  -1, -1):
+                x[i] = alpha[i-1] * x[i+1] + beta[i-1]
             return x
 
         def a_func(x):
@@ -105,7 +103,7 @@ class mathpart(Ui_MainWindow):
             elif x-h >= ksi:
                 return 2
             elif x-h<ksi and x > ksi:
-                return h/((ksi-x)/2 + h)
+                return h/((ksi-x+h) + (x-ksi)/2)
         def d_func(x):
             nonlocal h
             if x+h/2 <= ksi:
@@ -124,12 +122,6 @@ class mathpart(Ui_MainWindow):
             elif x-h/2 >= ksi:
                 return np.sqrt(2)/2
 
-            
-        # def intu(x):
-        #     return 2*x**3/3 + 5*x**2/2 + 7*x
-
-        # def du(x):
-        #     return 4*x + 5
 
         h = 1/n
         v = np.zeros(n+1)
@@ -138,34 +130,22 @@ class mathpart(Ui_MainWindow):
         d[0] = 1
         for i in range(1, n):
             d[i] = -f(i*h)
-            #d[i] = -(q(i*h)/h)*(intu(i*h+0.5*h) - intu(i*h-0.5*h)) + (k(i*h)/h) *(du(i*h+0.5*h) - du(i*h-0.5*h))
         d[n] = 0
 
-        # syst = np.zeros((n+1, n+1))
-        # syst[0][0] = 1
         C = np.zeros(n-1)
         A = np.zeros(n-1)
         B = np.zeros(n-1)
-        for i in range(1, n): #последний индекс - n-1
+        for i in range(1, n): #последний индекс - n-2
             xi = (i)*h
             xi1 = (i+1)*h
             A[i-1] = a_func(xi)/h**2
-            C[i-1] = -(a_func(xi)/(h**2)+d_func(xi)+a_func(xi1)/(h**2))
+            C[i-1] = (a_func(xi)/(h**2)+d_func(xi)+a_func(xi1)/(h**2))
             B[i-1] = a_func(xi1)/h**2
-        # syst[n][n] = 0
 
-        # v = np.linalg.solve(syst, d)
-        
-        # for i in range(0, n):
-        #     a[i] = k(i*h)/(h**2)
-        #     c[i+1] = -(k((i+1)*h) + k(i*h))/(h**2) + q(i*h)
-        #     b[i] = k((i+1)*h)/(h**2)
-        # c[0] = 1
-        # c[n] = 1
 
         v = TDMASolve(A, C, B, d)
         
-        plt.subplot(212)
+        plt.subplot(111)
         #plt.plot(u)
         plt.ylabel("Температура")
         plt.plot(v)