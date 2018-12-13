19fc020d6dfc81d55391d4a8a97cb70178812453
Vladimir Loginov <morkovka1337@users.noreply.github.com>
2018-12-13 21:42:53 +0300

    accurate solution

--- math_part.py
+++ math_part.py
@@ -23,21 +23,18 @@ class mathpart(Ui_MainWindow):
             return (h**2)*(12*(i**2)+1)/3 + 10*i*h + 6
 
 
-        def acc_sol(self, x):
+        def acc_sol(x):
             return 2*x**2 + 5*x + 7
 
         h = 1/n
         v = np.zeros(n+1)
-        u = np.zeros(n+1)
-        for i in range(0, n+1):
-            u[i] = acc_sol(self, i * h)
+        u = [acc_sol(i * h) for i in range(0, n+1)]
         
         
         b = np.zeros(n+1)
         syst = np.zeros((n+1, n+1))
         b[0] = 7
         for i in range(1, n):
-            #b[i] = -phi(self, i)
             b[i] = -4*((i*h)**2) - 10*i*h - 6 
         b[n] = 14
 
@@ -78,13 +75,13 @@ class mathpart(Ui_MainWindow):
         ksi = np.pi/4
         def TDMASolve(a, c, b, d):
             nonlocal n
-            alpha = np.zeros(n-1)
-            beta = np.zeros(n-1)
+            alpha = np.zeros(n)
+            beta = np.zeros(n)
             x = np.zeros(n+1)
             #x[0] = d[0] #x[0] = kapa1*x[1] + mu1, mu1 = d[0]
             alpha[0] = 0 # alpha[0] = kapa1, kapa1 в этой задаче всегда 0
             beta[0] = d[0] # beta[0] = mu[1]
-            for i in range(0, n-2):
+            for i in range(0, n-1):
                 alpha[i+1] = b[i]/(c[i]-alpha[i]*a[i])
                 # phi[i] = d[i+1]
                 beta[i+1] = (d[i+1] + beta[i] * a[i])/(c[i] - alpha[i] * a[i])
@@ -93,7 +90,7 @@ class mathpart(Ui_MainWindow):
 
             x[-1] = d[-1]
             for i in range (n-1,  -1, -1):
-                x[i] = alpha[i-1] * x[i+1] + beta[i-1]
+                x[i] = alpha[i] * x[i+1] + beta[i]
             return x
 
         def a_func(x):
@@ -109,9 +106,9 @@ class mathpart(Ui_MainWindow):
             if x+h/2 <= ksi:
                 return ksi + 1
             elif x-h/2 < ksi and x+h/2 > ksi:
-                return (ksi+1)*(ksi-x+h/2)/h + np.pi**2*(x+h/2-ksi)/(2*h)
+                return (ksi+1)*(ksi-x+h/2)/h + np.pi**2*(x+h/2-ksi)/(8*h)
             elif x-h/2 >= ksi:
-                return np.pi**2/2
+                return np.pi**2/8
             
         def f(x):
             nonlocal h
@@ -121,7 +118,16 @@ class mathpart(Ui_MainWindow):
                 return (ksi-x+h/2)/h + np.sqrt(2)*(x+h/2-ksi)/(2*h)
             elif x-h/2 >= ksi:
                 return np.sqrt(2)/2
-
+        def acc_sol(x):
+            # [-0.15717387  0.59707472  0.00681621 -0.8470241 ]
+            c1 = -0.15717387
+            c2 = 0.59707472
+            c3 = 0.00681621
+            c4 = -0.8470241
+            if x < ksi:
+                return c1*np.exp((np.sqrt(np.pi*4+1)) * x) + c2*np.exp(-((np.sqrt(np.pi*4+1)) * x)) + 1/(np.pi/4+1) 
+            else:
+                return c3*np.exp(np.pi/2*x) + c4*np.exp(-np.pi/2*x) + np.sqrt(2) / np.pi**2
 
         h = 1/n
         v = np.zeros(n+1)
@@ -135,7 +141,7 @@ class mathpart(Ui_MainWindow):
         C = np.zeros(n-1)
         A = np.zeros(n-1)
         B = np.zeros(n-1)
-        for i in range(1, n): #последний индекс - n-2
+        for i in range(1, n): #последний индекс - n-1
             xi = (i)*h
             xi1 = (i+1)*h
             A[i-1] = a_func(xi)/h**2
@@ -145,12 +151,45 @@ class mathpart(Ui_MainWindow):
 
         v = TDMASolve(A, C, B, d)
         
+        pi = np.pi
+        sqrt = np.sqrt
+        exp = np.exp
+
+        syst = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        b = [0, 0, 0, 0]
+        syst[0][0] = 1
+        syst[0][1] = 1
+        b[0] = 1-1/(pi/4+1)
+
+        syst[1][2] = exp(pi/2)
+        syst[1][3] = exp(-pi/2)
+        b[1] = -sqrt(2)/(pi**2)
+
+        syst[2][0] = exp(sqrt(pi/4 + 1)*(pi/4))
+        syst[2][1] = exp(-sqrt(pi/4 + 1)*(pi/4)) 
+        syst[2][2] = -exp((pi**2)/8)
+        syst[2][3] = -exp(-(pi**2)/8)
+        b[2] = 4*sqrt(2)/(pi**2) - 1/(ksi+1)  
+
+        syst[3][0] = sqrt(pi/4 + 1)*exp(sqrt(pi/4 + 1)*(pi/4))
+        syst[3][0] = -exp(-sqrt(pi/4 + 1)*(pi/4))*sqrt(pi/4 + 1)
+        syst[3][2] = -exp((pi**2)/8)*pi
+        syst[2][2] = exp(-(pi**2)/8)*pi
+
+        
+
+        y = np.linalg.solve(syst, b)
+        print (y)
+        # c = [-0.15717387  0.59707472  0.00681621 -0.8470241 ]
+        u = [acc_sol(i*h) for i in range(0, n+1)]
+
         plt.subplot(111)
-        #plt.plot(u)
+        plt.plot(u)
+        #
         plt.ylabel("Температура")
         plt.plot(v)
         #plt.plot(u-v)
-        plt.legend("Численное решение")
+        plt.legend(("Точное решение", "Численное решение"))
         plt.show()
 
         
